2bb4864fa722cd2d1440a2fa15f39afba5680d19
DemiPixel <luke5227@gmail.com>
2015-10-29 17:05:37 -0700

    Added !ip :D

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var irc = require('tmi.js');
 var request = require('request');
 var sw = require('./steamweb.js')('7EDFF01931452753ABB0145CC12A3D49');
 var fs = require('fs');
+var ssq = require('ssq');
 
-var channel = '#sleepingbear123';
+var channel = '#demipixel';
 
 var options = {
   options: {
@@ -32,6 +33,8 @@ client.on('connected', function() {
 });
 
 var refreshList = [];
+var showIP = true;
+
 client.on('chat', function(c, user, message, self) {
   if (self) return;
   if (!message) return;
@@ -77,19 +80,68 @@ client.on('chat', function(c, user, message, self) {
       } else {
         mission = m;
         chat('Seting SleepingBear to Tour ' + tour + ' Mission ' + mission);
-        tourMissionUpdated();
+        saveSettings();
       }
     }
   } else if (lowermes.indexOf('has won the raffle!') != -1 && user.username == 'moobot') {
     var match = getMatches(/(.*?) has won/gi, message);
     var winner = match[0][1];
     chat('Congratulations @' + winner + '!');
+  } else if (lowermes.indexOf('!ip') == 0) {
+    if (showIP) chatIP();
+    else chat('!ip is currently disabled.')
+  } else if (lowermes.indexOf('!showip') == 0 && isAdmin(user)) {
+    showIP = true;
+    chat('You can now type !ip to see the IP of the server Sleeping Bear is on.');
+    saveSettings();
+  } else if (lowermes.indexOf('!hideip') == 0 && isAdmin(user)) {
+    showIP = false;
+    chat('!ip is now disabled.');
+    saveSettings();
   }
 });
 
 function isAdmin(user) {
   user = user.username || user;
   return (user == 'demipixel' || user == 'sleepingbear123');
+}
+
+lastIP = 0;
+var cacheServer = '';
+var cacheServerData = ''
+
+function chatIP() {
+  if (lastIP >= Date.now() - 1000*15) {
+    chat(getStringFromServer(cacheServer, cacheServerData));
+  } else {
+    lastIP = Date.now();
+    sw.summary('76561198046453101', function(err, players) {
+      var server = players.players[0].gameserverip;
+      if (!server) {
+        chat(getStringFromServer());
+        cacheServer = '';
+        cacheServerData = null;
+      } else {
+        var serverSplit = server.split(':');
+        var ip = serverSplit[0];
+        var port = serverSplit[1];
+        ssq.info(ip, port, function(err, data) {
+          chat(getStringFromServer(server, data));
+          cacheServer = server;
+          cacheServerData = data;
+        });
+      }
+    });
+  }
+}
+
+function getStringFromServer(server, data) {
+  if (!server) return 'Sleeping Bear is not on any server!';
+  var str = server + '\n';
+  str += ' (' + data.numplayers + '/' + data.maxplayers;
+  str += '&nbsp' + data.map;
+  str += ') ' + data.servername.replace(/ \(.*? srcds.*? #.*?\)/gi, '').replace('Mann Up ', '').replace(/ /g, '&nbsp');
+  return str;
 }
 
 /*client.on('whisper', function(username, message) {
@@ -106,7 +158,7 @@ var mission = -1;
 
 function getTour() {
   sw.items(440, '76561198046453101', function(err, items) {
-    if (!items) {
+    if (!items || !items.items) {
       console.log('Fail item get');
       return;
     }
@@ -125,7 +177,7 @@ function getTour() {
           if (mission != 4) chat('SleepingBear has completed Tour ' + tour + ' Mission ' + mission  + '!');
           else chat('SleepingBear has completed Tour ' + tour + '!');
           mission++;
-          tourMissionUpdated();
+          saveSettings();
         }
       }
     }
@@ -135,16 +187,16 @@ function getTour() {
       if (tour) {
         mission = 1;
         console.log('Got new tour level: ' + tour);
-        tourMissionUpdated();
+        saveSettings();
       }
       tour = t;
     }
   });
 }
 
-function tourMissionUpdated() {
+function saveSettings() {
   if (tour == 0 || mission == -1) return;
-  fs.writeFile('./tour', tour + ',' + mission);
+  fs.writeFile('./tour', tour + ',' + mission + ',' + showIP);
 }
 
 var info = fs.existsSync('./tour') ? fs.readFileSync('./tour') : false;
@@ -153,6 +205,7 @@ if (info) {
   var split = info.toString().split(',');
   tour = split[0];
   mission = split[1];
+  showIP = typeof split[2] == 'undefined' ? true : split[2] == 'true';
 }
 getTour();
 setInterval(getTour, 1000*20);
