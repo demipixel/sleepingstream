eb87993326037abc1e8e267562635b0e79e662e8
DemiPixel <luke5227@gmail.com>
2015-11-08 14:31:38 -0800

    Big update!

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ var sw = require('./steamweb.js')('7EDFF01931452753ABB0145CC12A3D49');
 var fs = require('fs');
 var ssq = require('ssq');
 
-var channel = '#sleepingbear123';
+var ircs = ['#sleepingbear123', '#manofsnow'];
+var channels = ['sleepingbear123', 'manofsnow']
+var nicknames = ['Sleeping Bear', 'Snow'];
+var steamids = ['76561198046453101', '76561198005475714'];
+var data = {};
 
 var options = {
   options: {
@@ -18,7 +22,7 @@ var options = {
     username: 'demibotxel',
     password:'oauth:glxfqfv0dg990c0qpw0elhpb3k5ofr'
   },
-  channels: [channel]
+  channels: ircs
 };
 
 var whisperoptions = {
@@ -30,8 +34,9 @@ var whisperoptions = {
   identity: options.identity
 }
 
-function chat(msg) {
-  client.say(channel, msg);
+function chat(channel, msg) {
+  //client.say(channel, msg);
+  console.log(channel,msg)
 }
 
 function whisper(user, msg) {
@@ -42,8 +47,7 @@ var client = new irc.client(options);
 var whisperclient = new irc.client(whisperoptions);
 
 client.on('connected', function() {
-  //chat('Hello');
-  //chat('Global Announcement: @demipixel has mined the Rune Stone at 230,630! Type !demigame to join!');
+  console.log('Connected to channels');
 });
 
 whisperclient.on('connected', function() {
@@ -51,7 +55,6 @@ whisperclient.on('connected', function() {
 });
 
 var refreshList = [];
-var showIP = true;
 
 client.on('chat', function(c, user, message, self) {
   if (self) return;
@@ -59,16 +62,21 @@ client.on('chat', function(c, user, message, self) {
   var me = user.username == 'demipixel';
   var lowermes = message.toLowerCase();
 
+  var channelId = ircs.indexOf(c);
+  var channel = channels[channelId];
+  var nickname = nicknames[channelId];
+  var id = steamids[channelId];
+
   refreshList.push(lowermes.indexOf('refresh') > -1 ? 1 : 0);
   if (refreshList.length > 6) refreshList.splice(0, 1);
 
   var totalRefreshComments = refreshList.reduce((p, c) => p + c);
   if (totalRefreshComments >= 4) {
-    chat('**If the stream froze** PAUSE AND RESUME or CHANGE YOUR QUALITY SETTINGS')
+    chat(c,'**If the stream froze** PAUSE AND RESUME or CHANGE YOUR QUALITY SETTINGS')
   }
 
   if (lowermes.indexOf('!demibot') == 0) {
-    chat('Hey @' + user.username + ' !');
+    chat(c,'Hey @' + user.username + ' !');
   } else if (message.match(/#[^ #]*(demipixel|demi)[^ #]*/i) && !me) {
     var match = getMatches(/#([^ #]*(demipixel|demi)[^ #]*)/gi, message)
     var str = '';
@@ -76,48 +84,48 @@ client.on('chat', function(c, user, message, self) {
       str += '#' + match[m][1].replace(/(demipixel|demi)(bot)?/gi, user.username) + ' ';
     }
     str = str.trim();
-    chat(str);
+    chat(c,str);
   } else if (lowermes.indexOf('!joke') == 0) {
     request('http://api.yomomma.info/', function(err, body) {
-      var joke = JSON.parse(body.body).joke.replace(/m(o|a)(m|mm)a/gi, 'boy Sleeping Bear').replace(/she/gi, 'he').replace(/her/gi, 'his');
-      chat(joke);
+      var joke = JSON.parse(body.body).joke.replace(/m(o|a)(m|mm)a/gi, 'boy ' + nickname).replace(/she/gi, 'he').replace(/her/gi, 'his');
+      chat(c,joke);
     });
   } else if (lowermes.match(/deez( )?nut(s|z)/) && !me) {
-    chat('@' + user.username + ' needs to find funnier jokes.');
+    chat(c,'@' + user.username + ' needs to find funnier jokes.');
   } else if (lowermes.indexOf('!extrasongrequest') == 0) {
     var str = message.replace('extra', '');
-    chat(str);
+    chat(c,str);
   } else if (lowermes.indexOf('!tour') == 0 || lowermes.indexOf('!mission') == 0) {
-    if (mission == -1) chat('SleepingBear is on Tour ' + tour + '!');
-    else chat ('SleepingBear is on Tour ' + tour + ' Mission ' + mission + '!');
+    if (data[id].mission == -1) chat(c, nickname + ' is on Tour ' + data[id].tour + '!');
+    else chat (c, nickname + ' is on Tour ' + data[id].tour + ' Mission ' + data[id].mission + '!');
   } else if (lowermes.indexOf('!setmission ') == 0 && isAdmin(user)) {
     var m = parseInt(lowermes.replace('!setmission ', ''));
     if (m || m == 0) {
       if (m > 4 || m < 1) {
-        chat('Mission must be between 1 and 4');
+        chat(c,'Mission must be between 1 and 4');
       } else {
-        mission = m;
-        chat('Seting SleepingBear to Tour ' + tour + ' Mission ' + mission);
+        data[id].mission = m;
+        chat(c,'Seting ' + nickname + ' to Tour ' + data[id].tour + ' Mission ' + data[id].mission);
         saveSettings();
       }
     }
-  } else if (lowermes.indexOf('has won the raffle!') != -1 && user.username.toLowerCase() == 'moobot') {
+  } else if (lowermes.indexOf('has won the raffle!') != -1) {
     var match = getMatches(/(.*?) has won/gi, message);
     var winner = match[0][1];
-    chat('Congratulations @' + winner + '!');
+    chat(c,'Congratulations @' + winner + '!');
   } else if (lowermes.indexOf('!ip') == 0) {
-    if (showIP) chatIP();
-    else chat('!ip is currently disabled.')
+    if (data[id].showIP) chatIP(c, id, nickname);
+    else chat(c,'!ip is currently disabled.')
   } else if (lowermes.indexOf('!showip') == 0 && isAdmin(user)) {
-    showIP = true;
-    chat('You can now type !ip to see the IP of the server Sleeping Bear is on.');
+    data[id].showIP = true;
+    chat(c,'You can now type !ip to see the IP of the server ' + nickname + ' is on.');
     saveSettings();
   } else if (lowermes.indexOf('!hideip') == 0 && isAdmin(user)) {
-    showIP = false;
-    chat('!ip is now disabled.');
+    data[id].showIP = false;
+    chat(c,'!ip is now disabled.');
     saveSettings();
   } else if (lowermes.indexOf('!songlisttime') == 0) {
-    request('http://api.twitch.moobot.tv/1/channel/songrequests/playlist?channel=sleepingbear123', function(err, http, body) {
+    request('http://api.twitch.moobot.tv/1/channel/songrequests/playlist?channel=' + channel, function(err, http, body) {
       var songs = JSON.parse(body);
       var time = 0;
       songs.forEach((song) => time += song.length);
@@ -134,16 +142,16 @@ client.on('chat', function(c, user, message, self) {
       } else {
         str += hours + ':' + minutes + ':' + seconds;
       }
-      chat(str);
+      chat(c,str);
     });
   } else if (lowermes.indexOf('!souls') == 0) {
-    if (souls == 0) {
-      chat('Unknown number of souls');
+    if (data[id].souls == 0) {
+      chat(c,'Unknown number of souls');
     } else {
-      chat('Sleeping Bear has ' + souls + ' souls!');
+      chat(c, nickname + ' has ' + data[id].souls + ' souls!');
     }
   } else if (lowermes.indexOf('!tickets') == 0) {
-    chat('Sleeping Bear has ' + tickets + ' tickets!');
+    chat(c, nickname + ' has ' + tickets + ' tickets!');
   } else if (lowermes.indexOf('!timeuntil ') == 0) {
     var songtext = message.replace('!timeuntil ', '').trim();
     song = '';
@@ -153,7 +161,7 @@ client.on('chat', function(c, user, message, self) {
     } else if (match = songtext.match(/.*?youtu.be\/(.*?)(?:$|&)/)) {
       song = match[1];
     } else song = songtext.toLowerCase();
-    request('http://api.twitch.moobot.tv/1/channel/songrequests/playlist?channel=sleepingbear123', function(err, http, body) {
+    request('http://api.twitch.moobot.tv/1/channel/songrequests/playlist?channel='+channel, function(err, http, body) {
       var songs = JSON.parse(body);
       var waittime = 0;
       for (var s = 0; s < songs.length; s++) {
@@ -165,68 +173,55 @@ client.on('chat', function(c, user, message, self) {
           if (minutes < 10) minutes = '0' + minutes;
 
           if (hours == 0) {
-            chat('There is ' + minutes + ':' + seconds + ' until "' + songs[s].title + '"');
+            chat(c,'There is ' + minutes + ':' + seconds + ' until "' + songs[s].title + '"');
           } else {
-            chat('There is ' + hours + ':' + minutes + ':' + seconds + ' until "' + songs[s].title + '"');
+            chat(c,'There is ' + hours + ':' + minutes + ':' + seconds + ' until "' + songs[s].title + '"');
           }
           return;
         } else {
           waittime += songs[s].length;
         }
       }
-      chat('Could not find that song!');
+      chat(c,'Could not find that song!');
     });
   } else if (lowermes == '!500') {
-    chat('!songrequest 500 miles');
+    chat(c,'!songrequest https://www.youtube.com/watch?v=84CPo4bVkMk');
   } else if (lowermes.indexOf('!duck') == 0) {
     if (user.username.toLowerCase() == 'faayyuul') chat('QUACK!!!');
-    else chat('No');
+    else chat(c,'No');
   }
 });
 
 whisperclient.on('whisper', function(user, message) {
   var username = user.toLowerCase();
   if (message.indexOf('say ') != -1 && isAdmin(username)) {
-    chat(message.replace('say ', ''));
+    chat(c,message.replace('say ', ''));
   }
 });
 
 function isAdmin(user) {
   user = typeof user == 'object' ? user.username : user;
-  return (user == 'demipixel' || user == 'sleepingbear123');
+  return (user == 'demipixel' || user == 'sleepingbear123' || user == 'manofsnow');
 }
 
-lastIP = 0;
-var cacheServer = '';
-var cacheServerData = ''
-
-function chatIP() {
-  if (lastIP >= Date.now() - 1000*15) {
-    chat(getStringFromServer(cacheServer, cacheServerData));
-  } else {
-    lastIP = Date.now();
-    sw.summary('76561198046453101', function(err, players) {
-      var server = players.players[0].gameserverip;
-      if (!server) {
-        chat(getStringFromServer());
-        cacheServer = '';
-        cacheServerData = null;
-      } else {
-        var serverSplit = server.split(':');
-        var ip = serverSplit[0];
-        var port = serverSplit[1];
-        ssq.info(ip, port, function(err, data) {
-          chat(getStringFromServer(server, data));
-          cacheServer = server;
-          cacheServerData = data;
-        });
-      }
-    });
-  }
+function chatIP(c, id, nick) {
+  sw.summary(id, function(err, players) {
+    var server = players.players[0].gameserverip;
+    if (!server) {
+      chat(c,getStringFromServer(null,null,nick));
+    } else {
+      var serverSplit = server.split(':');
+      var ip = serverSplit[0];
+      var port = serverSplit[1];
+      ssq.info(ip, port, function(err, data) {
+        chat(c,getStringFromServer(server, data));
+      });
+    }
+  });
 }
 
-function getStringFromServer(server, data) {
-  if (!server) return 'Sleeping Bear is not on any server!';
+function getStringFromServer(server, data, nick) {
+  if (!server) return nick + ' is not on any server!';
   var str = server + '\n';
   str += ' (' + data.numplayers + '/' + data.maxplayers;
   str += '&nbsp' + data.map;
@@ -242,13 +237,8 @@ function getStringFromServer(server, data) {
   }
 });*/
 
-var tour = 0;
-var tickets = 0;
-var mission = -1;
-var souls = 0;
-
-function getTour() {
-  sw.items(440, '76561198046453101', function(err, items) {
+function getTour(c, id, nickname) {
+  sw.items(440, id, function(err, items) {
     if (!items || !items.items) {
       console.log('Fail item get');
       return;
@@ -258,51 +248,69 @@ function getTour() {
     var t = items.filter((item) => item.defindex == '1066')[0].level;
     var s = items.filter((item) => item.defindex == '5826')[0].attributes[0].value;
     
-    if (s) souls = s;
+    if (s) data[id].souls = s;
 
-    if (!tickets) {
+    if (!data[id].tickets) {
       
-    } else if (ticks > tickets) {
-      chat('SleepingBear bought ' + (ticks - tickets) + ' Tour of Duty tickets. He now has ' + ticks + ' tickets total.');
-    } else if (ticks == tickets - 1) {
-      if (mission != -1) {
-        if (mission > 4) mission = -1;
+    } else if (ticks > data[id].tickets) {
+      chat(c, nickname + ' bought ' + (ticks - data[id].tickets) + ' Tour of Duty tickets. He now has ' + ticks + ' tickets total.');
+    } else if (ticks == data[id].tickets - 1) {
+      if (data[id].mission != -1) {
+        if (data[id].mission > 4) data[id].mission = -1;
         else {
-          if (mission != 4) chat('SleepingBear has completed Tour ' + tour + ' Mission ' + mission  + '!');
-          else chat('SleepingBear has completed Tour ' + tour + '!');
-          mission++;
+          if (data[id].mission != 4) chat(c, nickname + ' has completed Tour ' + data[id].tour + ' Mission ' + data[id].mission  + '!');
+          else chat(c, nickname + ' has completed Tour ' + data[id].tour + '!');
+          data[id].mission++;
           saveSettings();
         }
       }
     }
-    tickets = ticks;
+    data[id].tickets = ticks;
 
-    if (parseInt(t) && tour != t) {
-      if (tour) {
-        mission = 1;
-        console.log('Got new tour level: ' + tour);
+    if (parseInt(t) && data[id].tour != t) {
+      if (data[id].tour) {
+        data[id].mission = 1;
+        console.log(nickname + ' got new tour level: ' + data[id].tour);
         saveSettings();
       }
-      tour = t;
+      data[id].tour = t;
     }
   });
 }
 
 function saveSettings() {
-  if (tour == 0 || mission == -1) return;
-  fs.writeFile('./tour', tour + ',' + mission + ',' + showIP);
+  fs.writeFile('./tour', JSON.stringify(data));
+}
+
+function tourInterval(channel, steamid, nickname) {
+  setInterval(function() {
+    getTour(channel, steamid, nickname);
+  }, 1000*20);
 }
 
 var info = fs.existsSync('./tour') ? fs.readFileSync('./tour') : false;
 if (info) {
-  console.log(info.toString()); 
-  var split = info.toString().split(',');
-  tour = split[0];
-  mission = split[1];
-  showIP = typeof split[2] == 'undefined' ? true : split[2] == 'true';
+  data = JSON.parse(info);
 }
-getTour();
-setInterval(getTour, 1000*20);
+for (var channel of channels) {
+  var c = channels.indexOf(channel);
+  var steamid = steamids[c];
+  var nickname = nicknames[c];
+  getTour(channel, steamid, nickname);
+
+  if (!data[steamid]) {
+    console.log('Creating data[' + steamid + ']');
+    data[steamid] = {
+      souls: 0,
+      mission: -1,
+      tour: 0,
+      tickets: 0,
+      showIP: true
+    }
+  }
+
+  tourInterval(channel, steamid, nickname)
+}
 
 function getMatches(re, s) {
   var m;
