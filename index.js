afc0ed6957025578d644c3a60177f39475397637
DemiPixel <luke5227@gmail.com>
2016-01-15 15:35:40 -0800

    Add !cat

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ client.on('chat', function(c, user, message, self) {
     else chat(c,'No');
   } else if (lowermes == '!bearluck') {
     var num = Math.floor(Math.random()*10) + 4;
-    if (Math.random() < 0.001) num = '1000000000';
+    if (Math.random() < 0.01) num = '1000000000';
     chat(c, '@'+user.username+': +'+num+' Unluckiness XP')
   } else if (lowermes.indexOf('!bearfact') == 0) {
     if (user == 'sleepingbear123') chat(c, 'Joke #3: Bears.');
@@ -216,6 +216,10 @@ client.on('chat', function(c, user, message, self) {
       var ind = Math.floor(Math.random() * BEAR_FACTS.length);
       chat(c, 'Bear Fact #'+(ind+1)+': '+BEAR_FACTS[ind]);
     }
+  } else if (lowermes.indexOf('!cat') == 0) {
+    var num = Math.floor(Math.random()*20) + 4;
+    if (Math.random() < 0.001) num = '1000000000';
+    chat(c, '@'+user.username+': +'+num+' Cat XP')
   }
 });
 
